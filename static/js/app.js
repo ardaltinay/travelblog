2cef32aa69ba3466658feb3df137786872195daf
Arda Altinay <ardaltinay48@gmail.com>
2020-08-10 15:06:58 +0300

    upgrade website

--- static/js/app.js
+++ static/js/app.js
@@ -1,28 +1,32 @@
-// navbara tıklayınca çalışan fonksiyon
+// function that is run when click on navbar
 var navbar = document.querySelector("header > i");
 navbar.onclick = function onClick () {
-	$("#hidden").hide().fadeIn(1200);
-	$("#hidden").css({"visibility": "visible", "pointer-events": "all"});
-	$("#hidden-bg").after( function hidden() {
-		$("#hidden-bg").css({"z-index": "100","pointer-events": "all", "visibility": "visible", "opacity": "0.75"})
-		$("#hidden-bg").hide().fadeIn(1200);
-	})
+    $("#hidden").hide().fadeIn(1200);
+    $("#hidden").css({"visibility": "visible", "pointer-events": "all"});
+    // $("#hidden").fadeIn(1200);
+    // $("#hidden").css({"display": "flex", "pointer-events": "all"}); // nav display should be none, visibility should be visible
+    $("#hidden-bg").css({"pointer-events": "all", "visibility": "visible"})
+    $("#hidden-bg").hide().fadeIn(1200);
 };
 
-// çarpı işaretiyle kapatılması
+// close function
+function close_box () {
+    $("#hidden").show().fadeOut(500);
+    $("#hidden-bg").show().fadeOut(500)
+}
+
+// navbar onclick
 var navbar_close = document.querySelector("#hidden > i");
 navbar_close.onclick = function closeClick () {
-	$("#hidden").show().fadeOut(500);
-	$("#hidden-bg").show().fadeOut(500)
+    close_box();
 };
 
-// boş alana tıklayınca kapatılması
+// outside of box onclick
 $("#hidden-bg").click(function closeNavbar () {
-	$("#hidden").show().fadeOut(500);
-	$("#hidden-bg").show().fadeOut(500)
+    close_box();
 });
 
 // animation
 $("document").ready(function animation () {
-	$(".main-content div").hide().fadeIn(1000);
+    $(".main-content div").hide().fadeIn(1000);
 });
