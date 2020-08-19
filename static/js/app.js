bdb099c045df9dc38ed5ce242e642d30136fc512
Arda Altinay <ardaltinay48@gmail.com>
2020-08-19 21:21:33 +0300

    added modal box and scss

--- static/js/app.js
+++ static/js/app.js
@@ -30,8 +30,66 @@ $(document).ready(function animation () {
     $(".main-content div").hide().fadeIn(1000);
 });
 
-// SLİDER CAROUSEL
 
+// MODAL POPUP BOX
+// create <a> tags for parents i and select their parents li
+$(".icons-content > div > ul > li:nth-child(n) > i").wrap("<a></a>");
+var createA = $(".icons-content > div > ul > li:nth-child(n) > a");
+var selectLi = $(".icons-content > div > ul > li:nth-child(n)");
+selectLi.css({"list-style": "none", "margin-top": "10px"});
+createA.css({"display": "inline-block", "cursor": "pointer"});
+
+// create a hidden div
+$(".icons-content").append("<div class='hidden-div'></div>");
+var hiddenDiv = $(".icons-content .hidden-div");
+hiddenDiv.css({"display": "none", "position": "fixed", "left": "50%", "top": "50%",
+    "transform": "translate(-50%, -50%)", "border": "2px solid #ee6588",
+    "background-color": "#fff", "width": "auto", "padding": "20px 0"});
+
+// create a hidden background
+$("body").prepend("<div class='hidden-modal-bg'></div>");
+var hiddenModalBg = $("body .hidden-modal-bg");
+hiddenModalBg.css({"position": "fixed", "display": "none", "width": "100%",
+    "height": "100%", "top": "0", "opacity": "0.7",
+    "background-color": "rgba(213, 213, 213, 0.7)"})
+
+// functions
+function closeModal () {
+    $(".icons-content .hidden-div .close").click(function clickClose () {
+        hiddenDiv.hide(500);
+        hiddenModalBg.fadeOut();
+        hiddenDiv.empty();
+        selectLi.css("pointer-events", "all");
+    });
+    hiddenModalBg.click(function clickBgClose () {
+        hiddenDiv.hide(500);
+        hiddenModalBg.fadeOut();
+        hiddenDiv.empty();
+        selectLi.css("pointer-events", "all");
+    });
+};
+
+selectLi.each(function (index) {
+    $(this).click(function () {
+        hiddenDiv.append("<span class='close'>&times;</span>", "<h2>Nisl feugiat adipiscing</h2>",
+        "<p>Lorem ipsum dolor sit amet nullam consequat, feugiat nisl tempus adipiscing sed cursus.</p>");
+        hiddenDiv.append($(this).clone());
+        $(".icons-content > div.hidden-div > li > a > i").css({"font-size": "36px",
+         "color": "#ee6588"});
+        hiddenDiv.fadeIn().css("z-index", "1000");
+        $(".icons-content .hidden-div .close").css({"position": "absolute",
+            "font-size": "40px", "right": "40px", "top": "0", "cursor": "pointer"})
+        $(".hidden-modal-bg").fadeIn();
+        selectLi.css("pointer-events", "none"); // for block more than 1 <li> in hidden-div
+        closeModal();
+    });
+});
+
+
+
+// SLİDER CAROUSEL, global variables
+var slider = $(".slider-content .slider-wrapper .slider");
+var articles = $(".slider-content .slider-wrapper .slider article");
 
 // functions
 function removeClassFromIndicators (indicators) {
@@ -44,12 +102,23 @@ function removeClassFromArticles (articles) {
         articles[i].classList.remove("active");
     }
 }
+function sliderMove (i) {
+    slider_wrapper_width = $('.slider-wrapper').width();
+    article_width = $('.slider-wrapper article').width();
+    article_margin = parseInt( $('.slider-wrapper article').css('marginLeft'));
+    slide_i = ( (slider_wrapper_width/2-article_width/2)-(i*(article_width+article_margin)+article_margin) );
+    if (i == 0) {
+        slide_i = 0;
+    } else if (i == articles.length - 1) {
+        slide_i = -(i+1)*(article_width+article_margin)+slider_wrapper_width-article_margin;
+    }
+    $(".slider").animate({"left": slide_i}, 1000);
+}
 
 
-// function that is run when page onload, variables
+// function that is run when page onload
 window.onload = function onLoad () {
-    var slider = $(".slider-content .slider-wrapper .slider");
-    var articles = $(".slider-content .slider-wrapper .slider article");
+
     articles[0].classList.add("active");
     $indicators_div = $('<div class="indicators" />').appendTo(".slider-wrapper")
 
@@ -68,28 +137,7 @@ window.onload = function onLoad () {
             removeClassFromArticles(articles);
             articles[index].classList.add("active");
 
-            /* slide_i = -(295 * 2 + (indicators.length -3) * 515);
-            if (index == 0) {
-                $(".slider").animate({"left": 0}, 1000);
-            } else if (index == 1) {
-                $(".slider").animate({"left": -295}, 1000);
-            } else if (1 < index && index < indicators.length - 1) {
-                $(".slider").animate({"left": index * (-515) + 220 }, 1000);
-            } else if (index == indicators.length - 1) {
-                $(".slider").animate({"left": slide_i}, 1000)
-            } */
-
-            slider_wrapper_width = $('.slider-wrapper').width();
-            article_width = $('.slider-wrapper article').width();
-            article_margin = parseInt( $('.slider-wrapper article').css('marginLeft'));
-            slide_i = ( (slider_wrapper_width/2-article_width/2)-(index*(article_width+article_margin)+article_margin) );
-            if (index == 0) {
-                slide_i = 0;
-            } else if (index == indicators.length - 1) {
-                slide_i = -(index+1)*(article_width+article_margin)+slider_wrapper_width-article_margin;
-            }
-            $(".slider").animate({"left": slide_i}, 1000);
-
+            sliderMove(index);
         });
     });
     slider.children('article').each(function (index) {
@@ -100,41 +148,10 @@ window.onload = function onLoad () {
             removeClassFromArticles(articles);
             $(this).addClass("active");
 
-            slide_i = -(295 * 2 + (indicators.length -3) * 515);
-            if (index == 0) {
-                $(".slider").animate({"left": 0}, 1000);
-            } else if (index == 1) {
-                $(".slider").animate({"left": -295}, 1000);
-            } else if (1 < index && index < indicators.length - 1) {
-                $(".slider").animate({"left": index * (-515) + 220 }, 1000);
-            } else if (index == indicators.length - 1) {
-                $(".slider").animate({"left": slide_i}, 1000)
-            }
+            sliderMove(index);
         });
     });
 };
-    //  functions that is run when clicks indicators
-    /*indicators[0].onclick = function clickIndicators () {
-        $(".slider").animate({"left": 0}, 1000); // first 295 px then add 515 px
-
-    };
-    indicators[1].onclick = function clickIndicators () {
-        $(".slider").animate({"left": -295}, 1000);
-
-    };
-    indicators[2].onclick = function clickIndicators () {
-        $(".slider").animate({"left": -810}, 1000);
-
-    };
-    indicators[3].onclick = function clickIndicators () {
-        $(".slider").animate({"left": -1325}, 1000);
-
-    };
-    indicators[4].onclick = function clickIndicators () {
-        $(".slider").animate({"left": -1620}, 1000);
-
-    };*/
-
 
 
 
